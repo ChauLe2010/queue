86c24d611a9fe4e39dbf8f1592c3c0d96f1d0725
chau.le <chau.le@codegym.vn>
2020-04-05 10:17:02 +0700

    init

---
+++ src/MyQueue.java
@@ -0,0 +1,89 @@
+import java.util.Queue;
+
+public class MyQueue {
+    private int capacity;
+    private int queueArr[];
+    private int head = 0;
+    private int tail = -1;
+    private int currentSize = 0;
+
+    public MyQueue(int queueSize) {
+        this.capacity = queueSize;
+        queueArr = new int[this.capacity];
+    }
+    public boolean isQueueFull(){
+        boolean status = false;
+        if (currentSize == capacity){
+            status = true;
+        }
+        return status;
+    }
+    public boolean isQueueEmpty(){
+        boolean status = false;
+        if (currentSize == 0){
+            status = true;
+        }
+        return status;
+    }
+    public void enqueue(int item) {
+        if (isQueueFull()) {
+            System.out.println("Overflow ! Unable to add element: " + item);
+        } else {
+            tail++;
+            if (tail == capacity ) {
+                tail = 0;
+            }
+            queueArr[tail] = item;
+            currentSize++;
+            System.out.println("Element " + item + " is pushed to Queue !");
+        }
+    }
+    public void dequeue() {
+        System.out.println();
+        if (isQueueEmpty()) {
+            System.out.println("Underflow ! Unable to remove element from Queue");
+        } else {
+            head++;
+            if (head == capacity - 1) {
+                System.out.println("Pop operation done ! removed: " + queueArr[head - 1]);
+                head = 0;
+            } else {
+                System.out.println("Pop operation done ! removed: " + queueArr[head - 1]);
+            }
+            currentSize--;
+        }
+    }
+    public void display(){
+        System.out.println();
+        for(int i=0;i<queueArr.length;i++){
+            System.out.print(queueArr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue(4);
+        queue.enqueue(4);
+        queue.display();
+        queue.dequeue();
+        queue.display();
+       queue.enqueue(56);
+        queue.display();
+       queue.enqueue(2);
+        queue.display();
+       queue.enqueue(67);
+        queue.display();
+        queue.enqueue(50);
+        queue.display();
+      queue.dequeue();
+        queue.display();
+        queue.dequeue();
+       queue.enqueue(24);
+        queue.dequeue();
+        queue.enqueue(98);
+        queue.enqueue(45);
+       queue.enqueue(23);
+        queue.enqueue(435);
+
+
+
+    }
+}
